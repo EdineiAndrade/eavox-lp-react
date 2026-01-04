c8e9e40b1739be7119af9acdcb744a65551a6eba
EdineiAndrade <edinei22.ba@gmail.com>
2026-01-04 17:47:10 -0300

    adicionando useEffect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,15 +15,27 @@ const Header = () => {
   return (
     <div>
       <header className="flex justify-between items-center p-4 px-4 lg:px20">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-light m-0 ">
+        <h1 data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500" className="text-3xl md:text-4xl lg:text-5xl font-light m-0">
             MCODE
         </h1>
         {/* DESKTOP NAVEGAÇÃO */}
-        <nav className="hidden md:flex items-center gap-12">
-            <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Sobre">Sobre</a>
-            <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Projetos">Projetos</a>
-            <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Tecnologias">Tecnologias</a>
-            <a className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Contato">Contato</a>
+            <nav data-aos="fade-down"
+            data-aos-easing="linear"
+            data-aos-duration="1000"className="hidden md:flex items-center gap-12">
+            <a data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1500" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Sobre">Sobre</a>
+            <a data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="2000" className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Projetos">Projetos</a>
+            <a data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="2500"className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Tecnologias">Tecnologias</a>
+            <a data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="3000"className="text-base tracking-wider transition-colors hover:text-gray-300 z-50" href="#Contato">Contato</a>
         </nav>
 
         <button className="hidden md:block bg-[#a7a7a7] text-black py-3 px-8 rounded-full border-none font-medium transition-all duration-500 hover:bg-white cursor-pointer z-50"> 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,9 @@ const Hero = () => {
  return (
     <main className="relative flex flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
 
-      <div className="max-w-xl ml-[5%] z-10">
+      <div data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine" className="max-w-xl ml-[5%] z-10">
         {/* Hero Text */}
         <div className="relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full flex">
           <div className="absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1 text-xs sm:text-sm">
